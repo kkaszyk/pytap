1e5e086ba5c0bd47398b6f18bf4e93c59e5d42ac
kuba <kubakaszyk@gmail.com>
2021-02-04 00:06:52 +0000

    adding program file

---
+++ program.py
@@ -0,0 +1,29 @@
+class Thread():
+    def __init__(self):
+        self.trace = []
+        self.waiting_for_mem = False
+        self.waiting_address = 0x0
+        self.barrier = False
+        self.pc = 0x0
+        self.address = 0x0
+        
+class Warp():
+    def __init__(self, warp_id):
+        self.threads = [Thread(),Thread(),Thread(),Thread()]
+        self.warp_id = warp_id
+        
+    def barrier(self):
+        count = 0
+        for t in self.threads:
+            if t.barrier:
+                count += 1
+        return count == len(self.threads)
+
+    def is_waiting_for_mem(self):
+        for t in self.threads:
+            if t.waiting_for_mem:
+                return True
+        return False
+    
+    def ready(self):
+        return not self.barrier() and not self.is_waiting_for_mem()
