68431857d430f028e2f08cf5527aa54094c52801
kuba <kubakaszyk@gmail.com>
2021-02-04 00:04:56 +0000

    packet print

--- packet.py
+++ packet.py
@@ -4,11 +4,17 @@ class InstructionPacket():
         self.num_instructions = num_instructions
         self.dependent_addrs = dependent_addrs
 
+    def print(self):
+        print("Arithmetic: " + str(self.num_instructions))
+
 class LoadPacket(InstructionPacket):
     def __init__(self, pc, num_instructions, dependent_addrs, bytes, address):
         super().__init__(pc, num_instructions, dependent_addrs)
         self.bytes = bytes
         self.address = address
+
+    def print(self):
+        print("Load: " + str(hex(self.address)))
 
 class StorePacket(InstructionPacket):
     def __init__(self, pc, num_instructions, dependent_addrs, bytes, address):
@@ -16,6 +22,12 @@ class StorePacket(InstructionPacket):
         self.bytes = bytes
         self.address = address
 
+    def print(self):
+        print("Store: " + str(hex(self.address)))
+
 class BarrierPacket(InstructionPacket):
     def __init__(self, pc, num_instructions, dependent_addrs):
         super().__init__(pc, num_instructions, dependent_addrs)
+
+    def print(self):
+        print("Barrier")
