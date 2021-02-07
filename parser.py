106ca669040c7f8a17813e1132c6f0c8c19f1bed
kuba <kubakaszyk@gmail.com>
2021-02-07 23:45:23 +0000

    parse n threadgroups on demand

--- parser.py
+++ parser.py
@@ -5,75 +5,79 @@ from packet import *
 from program import *
 
 class Parser():
-    def __init__(self):
+    def __init__(self, filename):
         self.warp_size = 4
         self.dimensions = []
-        self.trace = []
 
-        self.trace.append([])
-        self.trace[-1].append(Warp(0, self.warp_size))
-        self.warp_counter = 1
         self.tg_thread_counter = 0
-        self.warp_thread_counter = 0
 
-        self.thread_start_index = {}
-        self.thread_end_index = {}
+        self.threadgroup_start_index = {}
+        self.threadgroup_end_index = {}
+
+        self.threadgroup_id = 0
         
-    def index(self, filename):
+        self.filename = filename
+        self.index()
+        self.warp_counter = 0
+        
+    def index(self):
         line_count = 0
         thread_id = 0
+        threadgroup_id = 0
         line = 1
 
-        with open(filename, "r") as fp:
+        with open(self.filename, "r") as fp:
             while line:
                 line = fp.readline()
                 if line != "":
                     if line[0:2] == "JD":
                         self.dimensions = [int(x,16) for x in line.split(' ')[1:]]
                         self.tg_size = self.dimensions[3] * self.dimensions[4] * self.dimensions[5]
+                        self.num_tgs = self.dimensions[0] * self.dimensions[1] * self.dimensions[2]
+                        self.size = self.num_tgs * self.tg_size
                         self.warp_size = min(self.warp_size, self.tg_size)
                     elif line[0] == "T":
-                        if thread_id != 0:
-                            self.thread_end_index[thread_id - 1] = line_count - 1
-                            self.thread_start_index[thread_id] = line_count
-                            thread_id += 1
-                        else:
-                            self.thread_start_index[thread_id] = line_count
-                            thread_id += 1
+                        if (thread_id) % self.tg_size == 0 and thread_id != 0 and (((thread_id + 1) != self.size) or self.tg_size == 1):
+                            self.threadgroup_end_index[threadgroup_id - 1] = line_count
+                            self.threadgroup_start_index[threadgroup_id] = line_count
+                            threadgroup_id += 1
+                        elif thread_id == 0:
+                            self.threadgroup_start_index[threadgroup_id] = line_count
+                            threadgroup_id += 1
+                        thread_id += 1
                 line_count += 1
-        self.thread_end_index[thread_id - 1] = line_count
-        
-    def parse(self, filename):
-        self.index(filename)
-        
-        with open(filename, "r") as fp:
-            for i in self.thread_start_index.keys():
-                for line in itertools.islice(fp, self.thread_start_index[i], self.thread_end_index[i]):
+        self.threadgroup_end_index[threadgroup_id - 1] = line_count
+
+    def parse(self, n):
+        n = min(n, self.num_tgs - self.threadgroup_id)
+        trace = []
+        for i in list(self.threadgroup_start_index.keys())[self.threadgroup_id:self.threadgroup_id + n]:
+            with open(self.filename, "r") as fp:
+                trace.append([])
+                trace[-1].append(Warp(self.warp_counter, self.warp_size))
+                self.warp_counter += 1
+                warp_thread_counter = 0
+                tg_thread_counter = 0
+
+                for line in itertools.islice(fp, self.threadgroup_start_index[i], self.threadgroup_end_index[i]):
                     if line[0] == "T":
-                        if self.tg_thread_counter == self.tg_size:
-                            self.trace.append([])
-                            self.trace[-1].append(Warp(self.warp_counter-1, self.warp_size))
+                        if warp_thread_counter == self.warp_size:
+                            trace[-1].append(Warp(self.warp_counter, self.warp_size))
                             self.warp_counter += 1
-                            self.tg_thread_counter = 1
-                            self.warp_thread_counter = 1
-                            self.trace[-1][-1].add_thread()
-                        elif self.warp_thread_counter == self.warp_size:
-                            self.trace[-1].append(Warp(self.warp_counter-1, self.warp_size))
-                            self.warp_counter += 1
-                            self.warp_thread_counter = 1
-                            self.tg_thread_counter +=1
-                            self.trace[-1][-1].add_thread()
+                            warp_thread_counter = 1
+                            tg_thread_counter +=1
+                            trace[-1][-1].add_thread()
                         else:
-                            self.trace[-1][-1].add_thread()
-                            self.tg_thread_counter += 1
-                            self.warp_thread_counter += 1
+                            trace[-1][-1].add_thread()
+                            tg_thread_counter += 1
+                            warp_thread_counter += 1
                     elif line[0] == "P":
                         entry = [int(x,16) for x in line.split(' ')[1:]]
                         pc = entry[0]
                         num_instructions = entry[1]
                         num_deps = entry[2]
                         deps = entry[3:3+num_deps]
-                        self.trace[-1][-1].threads[self.warp_thread_counter - 1].trace.append(InstructionPacket(pc, num_instructions, deps))
+                        trace[-1][-1].threads[warp_thread_counter - 1].trace.append(InstructionPacket(pc, num_instructions, deps))
                     elif line[0] == "L":
                         entry = [int(x,16) for x in line.split(' ')[1:]]
                         bytes = entry[0]
@@ -82,7 +86,7 @@ class Parser():
                         num_instructions = entry[3]
                         num_deps = entry[4]
                         deps = entry[5:5+num_deps]
-                        self.trace[-1][-1].threads[self.warp_thread_counter - 1].trace.append(LoadPacket(pc, num_instructions, deps, bytes, address))
+                        trace[-1][-1].threads[warp_thread_counter - 1].trace.append(LoadPacket(pc, num_instructions, deps, bytes, address))
                     elif line[0] == "S":
                         entry = [int(x,16) for x in line.split(' ')[1:]]
                         bytes = entry[0]
@@ -91,20 +95,22 @@ class Parser():
                         num_instructions = entry[3]
                         num_deps = entry[4]
                         deps = entry[5:5+num_deps]
-                        self.trace[-1][-1].threads[self.warp_thread_counter - 1].trace.append(StorePacket(pc, num_instructions, deps, bytes, address))
+                        trace[-1][-1].threads[warp_thread_counter - 1].trace.append(StorePacket(pc, num_instructions, deps, bytes, address))
                     elif line[0] == "B":
                         entry = [int(x,16) for x in line.split(' ')[1:]]
                         pc = entry[0]
                         num_instructions = entry[1]
                         num_deps = entry[2]
                         deps = entry[3:3+num_deps]
-                        self.trace[-1][-1].threads[self.warp_thread_counter - 1].trace.append(BarrierPacket(pc, num_instructions, deps))
+                        trace[-1][-1].threads[warp_thread_counter - 1].trace.append(BarrierPacket(pc, num_instructions, deps))
 
-            return self.dimensions, self.trace
+        self.threadgroup_id += n
+        return trace
 
 def test():
-    p = Parser()
-    dimensions, trace = p.parse(sys.argv[1])
+    p = Parser(sys.argv[1])
+    trace = p.parse(int(sys.argv[2]))
+    print("TEST")
     for tg in trace:
         print("TG")
         for w in tg:
